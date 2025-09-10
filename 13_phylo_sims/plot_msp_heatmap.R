eb39596ed4a80582b7b9d2ef7bc2102b935ece86
Ethan Gyllenhaal <egyllenhaal@unm.edu>
2025-09-10 14:11:24 -0500

    Add files via upload

--- 13_phylo_sims/plot_msp_heatmap.R
+++ 13_phylo_sims/plot_msp_heatmap.R
@@ -1,26 +1,29 @@
 library(tidyverse)
 
+# set directory and read inputs
 setwd("C:/Documents/Projects/SoloSymposRad/arch_msp/")
 raw_input <- read.csv("summarize_buk.txt")
 raw_input_makira <- read.csv("summarize_mak.txt")
 
+# process input with different stats
 input <- raw_input %>% rowwise() %>%
-  mutate(count = sum(c_across(X...B.N..L..K.:Poor.resolution))) %>%
-  mutate(nres = sum(c_across(X...B.N..L..K.:X...L.K..N..B.))) %>%
-  mutate(true = X...L.K..B..N.) %>%
-  mutate(k_sister = X...B.N..L..K. + X...B.L..N..K. + X...N.L..B..K.) %>%
-  mutate(other = (count - k_sister - true - Non.monophyletic)) %>%
-  mutate(x = paste(disp, time, sep="_")) %>%
-  mutate(y = paste(ne, int, sep="_")) %>%
+  mutate(count = sum(c_across(X...B.N..L..K.:Poor.resolution))) %>% # count of finished trees
+  mutate(nres = sum(c_across(X...B.N..L..K.:X...L.K..N..B.))) %>% # count of resolved trees
+  mutate(true = X...L.K..B..N.) %>% # true topology
+  mutate(k_sister = X...B.N..L..K. + X...B.L..N..K. + X...N.L..B..K.) %>% # makira sister
+  mutate(other = (count - k_sister - true - Non.monophyletic)) %>% # other patterns
+  mutate(x = paste(disp, time, sep="_")) %>% # x axis pair
+  mutate(y = paste(ne, int, sep="_")) %>% # y axis pair
   mutate(summary = case_when((Non.monophyletic + Poor.resolution)/count > 0.5 ~ "poor_resolution", # + Poor.resolution for bootstrap version
                              (X...B.N..K..L. + X...B.K..N..L. + X...N.K..B..L.)/nres > 0.5 ~ "L_sis",
                              (X...B.N..L..K. + X...B.L..N..K. + X...N.L..B..K.)/nres > 0.5 ~ "K_sis",
                              (X...B.L..K..N. + X...L.K..B..N. + X...B.K..L..N.)/nres > 0.5 ~ "N_sis",
                              (X...N.L..K..B. + X...N.K..L..B. + X...L.K..N..B.)/nres > 0.5 ~ "B_sis",
                              (X.B.K...N.L. + X.B.N...L.K. + X.B.L...N.K.)/nres > 0.5 ~ "balanced", 
-                             .default = "mixed")) %>%
-  arrange(disp, ne, time, int)
-# switch to this to separate different Makira sister ####
+                             .default = "mixed")) %>% # make summaries for plot
+  arrange(disp, ne, time, int) # sortting it
+
+# switch to this to separate different Makira sister patterns ####
  # input <- raw_input %>% rowwise() %>%
  #   mutate(count = sum(c_across(X...B.N..L..K.:Poor.resolution))) %>%
  #   mutate(nres = sum(c_across(X...B.N..L..K.:X...L.K..N..B.))) %>%
@@ -42,15 +45,17 @@ input <- raw_input %>% rowwise() %>%
  # 
 #color_assignments <- c("L_sis" = "#C1B49B", "K_sis1" = "#AA50B4", "K_sis2" = "violet", "K_sis3" = "hotpink",  "N_sis" = "#F06623", "B_sis" = "#009949", "poor_resolution" = "black", "balanced" = "#E6B400", "mixed" = "#1496E6")
 #####
-
+# color assignments
 color_assignments <- c("L_sis" = "#C1B49B", "K_sis" = "#AA50B4", "N_sis" = "#F06623", "B_sis" = "#009949", "poor_resolution" = "black", "balanced" = "#E6B400", "mixed" = "#1496E6")
 
+# full plot
 ggplot(input, aes(x=factor(disp), y=factor(ne))) + 
   geom_tile(aes(fill=summary, alpha=1-(true/count)), color = "white") + 
   facet_wrap(vars(factor(time), factor(int)), nrow=5) +
   scale_fill_manual(values = color_assignments) +
   theme(strip.background = element_blank(), strip.text.x = element_blank(), panel.background = element_blank(), panel.spacing = unit(0.2, "lines")) 
 
+# zoomed plot
 ggplot(filter(input, time<1000000 & int > 50000 & ! disp %in% c(50,100)), aes(x=factor(disp), y=factor(ne))) + 
   geom_tile(aes(fill=summary, alpha=1-(true/count)), color = "white") + 
   facet_wrap(vars(factor(time), factor(int)), nrow=3) +
@@ -59,6 +64,7 @@ ggplot(filter(input, time<1000000 & int > 50000 & ! disp %in% c(50,100)), aes(x=
 
 # Makira
 
+# same categories as Bukida first
 input_makira <- raw_input_makira %>% rowwise() %>%
   mutate(count = sum(c_across(X...B.N..L..K.:Poor.resolution))) %>%
   mutate(nres = sum(c_across(X...B.N..L..K.:X...L.K..N..B.))) %>%
@@ -103,76 +109,3 @@ fisher.test(ksis)
 
 summary_buk <- c(nrow(filter(input, summary=="K_sis")), nrow(filter(input, summary=="balanced")), nrow(filter(input, summary=="N_sis")))
 summary_mak <- c(nrow(filter(input_makira, summary=="K_sis")), nrow(filter(input_makira, summary=="balanced")), nrow(filter(input_makira, summary=="N_sis")))
-
-## JOBTALK
-
-input_talk <- raw_input %>%
-  mutate(true = X...L.K..B..N.) %>%
-  mutate(k_sister = X...B.N..L..K. + X...B.L..N..K. + X...N.L..B..K.) %>%
-  mutate(other = (50 - k_sister - true - Non.monophyletic)) %>%
-  mutate(x = paste(disp, ne, sep="_")) %>%
-  mutate(y = paste(time, int, sep="_")) %>%
-  mutate(summary = case_when(Non.monophyletic + Poor.resolution > 20 ~ "poor_resolution", # + Poor.resolution for bootstrap version
-                             X...B.N..K..L. + X...B.K..N..L. + X...N.K..B..L. > 30 ~ "L_sis",
-                             X...B.N..L..K. + X...B.L..N..K. + X...N.L..B..K. > 30 ~ "K_sis",
-                             X...B.L..K..N. + X...L.K..B..N. + X...B.K..L..N. > 30 ~ "N_sis",
-                             X...N.L..K..B. + X...N.K..L..B. + X...L.K..N..B. > 30 ~ "B_sis",
-                             X.B.K...N.L. + X.B.N...L.K. + X.B.L...N.K. > 30 ~ "balanced", 
-                             .default = "mixed")) %>%
-  arrange(disp, int, time, ne)
-
-#color_assignments <- c("L_sis" = "#C1B49B", "K_sis" = "#CC79A7", "N_sis" = "#D55E00", "B_sis" = "#61D04F", "poor_resolution" = "black", "balanced" = "#E69F00", "mixed" = "#0072B2")
-color_assignments <- c("L_sis" = "#C1B49B", "K_sis" = "#AA50B4", "N_sis" = "#F06623", "B_sis" = "#009949", "poor_resolution" = "black", "balanced" = "#E6B400", "mixed" = "#1496E6")
-
-ggplot(input_talk, aes(x=factor(disp), y=factor(int))) + 
-  geom_tile(aes(fill=summary, alpha=1-(true/50)), color = "white") + 
-  facet_wrap(vars(factor(time), factor(ne)), nrow=5) +
-  scale_fill_manual(values = color_assignments) +
-  theme(strip.background = element_blank(), strip.text.x = element_blank(), panel.background = element_blank(), panel.spacing = unit(0.2, "lines")) 
-
-
-## old test  ####
-
-true <- c(20,10,0,0,5,5)
-k <- c(0,10,20,10,5,15)
-other <- c(0,0,0,10,10,0)
-
-df <- data.frame(true, k, other)
-
-
-df_color <- df %>%
-  mutate(color = rgb(red=k/20, blue=other/20, green = 0, alpha = 1- true/20))
-
-x <- seq(1,6)
-y <- seq(1)
-grid <- expand.grid(x,y)
-grid$colors <- rep(df_color$color, 1)
-
-ggplot(grid, aes(x=Var1, y=Var2)) + geom_tile(aes(fill=colors)) + scale_fill_manual(values = unique(df_color$color))
-
-df$x <- grid$Var1
-df$y <- grid$Var2
-
-ggplot(df, aes(x=x, y=y)) + geom_tile(aes(fill=k, alpha=1-true)) + scale_fill_gradient(low = "black", high="purple")
-
-
-
-# other old versions
-
-
-ggplot(input, aes(x=factor(x), y=factor(y))) + 
-  geom_tile(aes(fill=k_sister - other, alpha=1-(true/20)), color = "white") + 
-  scale_fill_gradient2(low = "orange",  mid = "black", high="purple") +
-  theme_classic()
-
-ggplot(input, aes(x=factor(time), y=factor(int))) + 
-  geom_tile(aes(fill=k_sister/(k_sister+other), alpha=1-(true/20)), color = "white") + 
-  facet_wrap(vars(factor(ne), factor(disp)), nrow=3) +
-  scale_fill_gradient(low = "orange",  high="darkmagenta") +
-  theme(strip.background = element_blank(), strip.text.x = element_blank(), panel.background = element_blank(), panel.spacing = unit(0.2, "lines")) 
-
-ggplot(input, aes(x=factor(time), y=factor(int))) + 
-  geom_tile(aes(fill=summary, alpha=1-(true/20)), color = "white") + 
-  facet_wrap(vars(factor(ne), factor(disp)), nrow=3) +
-  scale_fill_manual(values = color_assignments) +
-  theme(strip.background = element_blank(), strip.text.x = element_blank(), panel.background = element_blank(), panel.spacing = unit(0.2, "lines")) 
